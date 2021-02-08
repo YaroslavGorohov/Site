97600aa2ae873a6f90c0582d2400a55bd6571a6f
YaroslavGorohov <yarikgorohov@gmail.com>
2021-02-08 14:25:59 +0300

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,3 @@
 dir %WORKSPACE%
-dir
-#
+
 "C:\Program Files\IIS\Microsoft Web Deploy V3\MSDeploy.exe" -verb:sync -preSync:runCommand="%WORKSPACE%\presync.bat" -source:iisapp="%WORKSPACE%" -dest:iisapp="test.onlyoffice.co",computerName="35.174.218.204",UserName=%USERNAME%,Password=%PASSWORD%, -postSync:runCommand="%WORKSPACE%\postsync.bat"
