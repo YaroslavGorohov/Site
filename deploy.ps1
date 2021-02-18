49862e48c281de52bc93b16d0f80dcc63c554668
YaroslavGorohov <yarikgorohov@gmail.com>
2021-02-18 17:52:02 +0300

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -2,6 +2,10 @@ get-psdrive
 echo  $env:SecretKey
 echo CVS_BRANCH
 echo $ENV:CVS_BRANCH
+echo 111
+echo $env:GIT_BRANCH
+echo 222
+echo $GIT_BRANCH
 echo 123
 #Set-AWSCredentials -AccessKey $Env:AccessKey -SecretKey $Env:SecretKey
 echo  %SecretKey%
